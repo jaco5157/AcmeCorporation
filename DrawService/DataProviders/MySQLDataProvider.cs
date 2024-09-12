500108ccc864156f384f5930d9d4a649452e49d0
Jacob <jacob-dan@hotmail.com>
2024-09-12 11:44:07 +0200

    feat: simple database implementation

--- DrawService/DataProviders/MySQLDataProvider.cs
+++ DrawService/DataProviders/MySQLDataProvider.cs
@@ -1,10 +1,36 @@
 using ClassLibrary.Models;
 using DrawService.Interfaces;
+using MySql.Data.MySqlClient;
 
 namespace DrawService.DataProviders;
 
 public class MySQLDataProvider : IDataProvider
 {
+    private string cs = @"server=database.draw;userid=root;password=;database=db";
+    public void setConnectionString(string connectionString) => cs = connectionString;
+    private readonly ILogger<MySQLDataProvider>? _logger;
+
+    public MySQLDataProvider(ILogger<MySQLDataProvider>? logger = null)
+    {
+        _logger = logger;
+    }
+    
+    private bool Open(MySqlConnection con)
+    {
+        try
+        {
+            con.Open();
+            return true;
+        }
+        catch (Exception err)
+        {
+            _logger?.LogError(err, "Failed to connect to DB");
+            return false;
+        }
+    }
+    
+    
+    
     public Draw? SubmitDraw(Draw draw)
     {
         throw new NotImplementedException();
